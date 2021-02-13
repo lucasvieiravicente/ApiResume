7d46ce111f9a002b99abe41260881a80eba1d737
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2021-02-13 16:11:41 -0300

    criado classe para configuração do startup e alteração do lauchsettings para iniciar no swagger

---
+++ Domain/Utils/ConfigStartup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using ApiResume.Domain.BlobContext.Interfaces;
+using ApiResume.Domain.Repository;
+using ApiResume.Domain.Repository.Interfaces;
+using ApiResume.Services;
+using ApiResume.Services.Interfaces;
+using ApiResume.Domain.Context;
+using BlobCont = ApiResume.Domain.BlobContext.BlobContext;
+
+namespace ApiResume.Domain.Utils
+{
+    public static class ConfigStartup
+    {
+        public static void ConfigureDatabaseConnection(IConfiguration configuration, IServiceCollection services)
+        {
+            var connection = configuration.GetConnectionString("ApiData");
+            services.AddDbContext<EFContext>(options => options.UseSqlServer(connection));
+        }
+
+        public static void ConfigureDI(IServiceCollection services)
+        {
+            services.AddScoped<IKnowledgeRepository, KnowledgeRepository>();
+            services.AddScoped<IBlobContext, BlobCont>();
+            services.AddScoped<IKnowledgeService, KnowledgeService>();
+            services.AddAutoMapper(typeof(ConfigMapper));
+        }
+
+        public static void ConfigureSwagger(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Knowledge V1");
+            });
+        }
+
+        public static void ConfigureSwaggerGenInfos(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "API Knowledge",
+                    Description = "A simple API to populate my webresume online with informations about my professional knowledge and study",
+                    License = new OpenApiLicense()
+                    {
+                        Name = "MIT License",
+                        Url = new Uri("https://github.com/lucasvieiravicente/WebResumeAngular/blob/main/LICENSE")
+                    },
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Lucas V. Vicente",
+                        Email = "lucasvieiravicente1@gmail.com",
+                        Url = new Uri("https://white-moss-0cf7e1e0f.azurestaticapps.net/")
+                    }
+                });
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,20 +1,9 @@
-using ApiResume.Domain.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using ApiResume.Services;
-using ApiResume.Services.Interfaces;
-using ApiResume.Domain.Repository.Interfaces;
-using ApiResume.Domain.Repository;
-using ApiResume.Domain.BlobContext.Interfaces;
-using ApiResume.Domain.BlobContext;
 using ApiResume.Domain.Utils;
-using Swashbuckle;
-using Microsoft.OpenApi.Models;
-using System;
 
 namespace ApiResume
 {
@@ -30,10 +19,9 @@ namespace ApiResume
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            ConfigureDI(services);
-            var connection = Configuration.GetConnectionString("ApiData");
-            services.AddDbContext<EFContext>(options => options.UseSqlServer(connection));
-            ConfigureSwaggerGenInfos(services);
+            ConfigStartup.ConfigureDI(services);
+            ConfigStartup.ConfigureSwaggerGenInfos(services);
+            ConfigStartup.ConfigureDatabaseConnection(Configuration, services);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -54,47 +42,7 @@ namespace ApiResume
                 endpoints.MapControllers();
             });
 
-            ConfigureSwagger(app);
-        }
-
-        private void ConfigureDI(IServiceCollection services)
-        {
-            services.AddScoped<IKnowledgeRepository, KnowledgeRepository>();
-            services.AddScoped<IBlobContext, BlobContext>();
-            services.AddScoped<IKnowledgeService, KnowledgeService>();
-            services.AddAutoMapper(typeof(ConfigMapper));
-        }
-
-        private void ConfigureSwagger(IApplicationBuilder app)
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Knowledge V1");
-            });
-        }
-
-        private void ConfigureSwaggerGenInfos(IServiceCollection services)
-        {
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo()
-                {
-                    Title = "API Knowledge",
-                    Description = "A simple API to populate my webresume online with informations about my carrer and study",
-                    License = new OpenApiLicense()
-                    {
-                        Name = "MIT License",
-                        Url = new Uri("https://github.com/lucasvieiravicente/WebResumeAngular/blob/main/LICENSE")
-                    },
-                    Contact = new OpenApiContact()
-                    {
-                        Name = "Lucas V. Vicente",
-                        Email = "lucasvieiravicente1@gmail.com",
-                        Url = new Uri("https://www.linkedin.com/in/lucas-vieira-vicente-885726127/")
-                    }
-                });
-            });
-        }
+            ConfigStartup.ConfigureSwagger(app);
+        }     
     }
 }
