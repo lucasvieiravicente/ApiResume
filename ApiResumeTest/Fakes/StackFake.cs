67b51a0310bb4e20527684525d366de2deaecc04
Lucas Vieira Vicente <lucas.vieira@easynvest.com.br>
2022-03-13 01:00:55 -0300

    Criando testes do StackService

---
+++ ApiResumeTest/Fakes/StackFake.cs
@@ -0,0 +1,42 @@
+﻿using ApiResume.Domain.Enums;
+using ApiResume.Domain.Models;
+using Bogus;
+using System.Collections.Generic;
+
+namespace ApiResumeTest.Fakes
+{
+    public struct StackFake
+    {
+        private const int NUMBER_OF_STACKS = 10;
+        private const string LOCALE_LANGUAGE = "pt_BR";
+
+        public static Stack GetStackById(StackIds stackId)
+        {
+            return new Faker<Stack>(LOCALE_LANGUAGE)
+                            .RuleFor(x => x.DateAdded, x => x.Date.Past())
+                            .RuleFor(x => x.DateModified, x => x.Date.Past())
+                            .RuleFor(x => x.StackId, x => stackId)
+                            .RuleFor(x => x.StackName, x => x.Lorem.Slug())
+                            .Generate();
+        }
+
+        public static Stack GetStack()
+        {
+            return new Faker<Stack>(LOCALE_LANGUAGE)
+                            .RuleFor(x => x.DateAdded, x => x.Date.Past())
+                            .RuleFor(x => x.DateModified, x => x.Date.Past())
+                            .RuleFor(x => x.StackId, x => (StackIds)x.Random.Number(1, 5))
+                            .RuleFor(x => x.StackName, x => x.Lorem.Slug())
+                            .Generate();
+        }
+
+        public static List<Stack> GetStacks()
+        {
+            var stacks = new List<Stack>();
+            for (int i = 0; i < NUMBER_OF_STACKS; i++)
+                stacks.Add(GetStack());
+
+            return stacks;
+        }     
+    }
+}
---
+++ ApiResumeTest/Services/Stacks/StackServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using ApiResume.Domain.Enums;
+using ApiResume.Domain.Models;
+using ApiResume.Domain.Repository.Interfaces;
+using ApiResume.Domain.Responses;
+using ApiResume.Services.Interfaces.Stacks;
+using ApiResume.Services.Stacks;
+using ApiResumeTest.Fakes;
+using AutoMapper;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApiResumeTest.Services.Stacks
+{
+    public class StackServiceTests
+    {
+        private readonly Mock<IMapper> _mapper;
+        private readonly Mock<IStackRepository> _stackRepository;
+        private readonly IStackService _stackService;
+
+        public StackServiceTests()
+        {
+            _mapper = new Mock<IMapper>();
+            _stackRepository = new Mock<IStackRepository>();
+            _stackService = new StackService(_mapper.Object, _stackRepository.Object);
+        }
+
+        [Fact]
+        public async Task ShouldReturnSuccess_WhenService_GetStackById()
+        {
+            // Arrange
+            var stackId = StackIds.BackEndFrameworks;
+            Stack stack = StackFake.GetStackById(stackId);
+            var stackResponse = new StackResponse { StackId = stack.StackId, StackName = stack.StackName };
+
+            _stackRepository.Setup(m => m.GetStackByStackId(It.IsAny<StackIds>())).Returns(Task.FromResult(stack));
+            _mapper.Setup(m => m.Map<StackResponse>(stack)).Returns(stackResponse);
+
+            // Act
+            StackResponse result = await _stackService.GetStackByStackId(stackId);
+
+            // Assert 
+            Assert.NotNull(result);
+            Assert.Equal(stack.StackId, result.StackId);
+            Assert.Equal(stack.StackName, result.StackName);
+        }
+
+        [Fact]
+        public async Task ShouldReturnSuccess_WhenService_GetAllStacks()
+        {
+            // Arrange
+            IEnumerable<Stack> stacks = StackFake.GetStacks();
+            IEnumerable<StackResponse> stackResponses = stacks.Select(stack =>
+            {
+                return new StackResponse { StackId = stack.StackId, StackName = stack.StackName };
+            });
+
+            _stackRepository.Setup(m => m.GetAll()).Returns(Task.FromResult(stacks));
+            _mapper.Setup(m => m.Map<IEnumerable<StackResponse>>(It.IsAny<IEnumerable<Stack>>())).Returns(stackResponses);
+
+            // Act
+            IEnumerable<StackResponse> result = await _stackService.GetAll();
+
+            // Assert 
+            Assert.NotEmpty(result);
+            Assert.Equal(result.Count(), stackResponses.Count());
+        }
+    }
+}
